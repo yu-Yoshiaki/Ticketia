f82359fdf9bb041be73b53fb64764ee61301153c
yoshiosmile <32384795+yu-Yoshiaki@users.noreply.github.com>
2022-04-28 15:54:34 +0900

    アップデート4

--- src/pages/ticket/layout/Checkout.tsx
+++ src/pages/ticket/layout/Checkout.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
-import { useRouter } from 'next/router';
+
 import type { VFC } from "react";
+import { useRouter } from 'next/router';
 import type { ReadPrice } from "src/type/ticket";
 
 
